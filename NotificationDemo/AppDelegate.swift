393181f90111172b8b2a3e8064f54561c6654ae2
Praveen <praveen@codilar.com>
2018-05-11 12:03:08 +0530

    first commit

---
+++ NotificationDemo/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  NotificationDemo
+//
+//  Created by Praveen on 09/05/18.
+//  Copyright © 2018 Praveen. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.sound, .alert, .badge])
+        print(notification.request.identifier)
+    }
+    
+}
+
---
+++ NotificationDemo/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  NotificationDemo
+//
+//  Created by Praveen on 09/05/18.
+//  Copyright © 2018 Praveen. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        checkNotificationAuthorization()
+        createNotification()
+    }
+    
+    
+    func createNotification() {
+        
+        //1 content
+        let content = UNMutableNotificationContent()
+        content.title = "Codilar Title"
+        content.subtitle = "Magento based ecommarece companmy"
+        content.body = "Swift uses Automatic Reference Counting (ARC) to track and manage your app’s memory usage. In most cases, this means that memory management “just works” in Swift, and you do not need to think about memory management yourself"
+        content.sound = UNNotificationSound.default()
+        
+        
+        //2 trigger
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5.0, repeats: false)
+        
+        //3 request
+        let request = UNNotificationRequest(identifier: "someID", content: content, trigger: trigger)
+        
+        //4 add notitfication to queue
+        UNUserNotificationCenter.current()
+            .add(request) { (error) in
+                if error != nil {
+                    print("Some error occured: ", error?.localizedDescription ?? "")
+                }else {
+                    print("Notification will show")
+                }
+        }
+        
+    }
+    
+    func checkNotificationAuthorization() {
+        UNUserNotificationCenter.current()
+            .requestAuthorization(options: [.alert, .sound]) {
+                (granted, error) in
+                if granted {
+                    print("Notification Permission granted")
+                } else {
+                    print("ERROR: ", error?.localizedDescription ?? "ERROR")
+                }
+        }
+    }
+    
+    
+    
+    
+
+}
+
